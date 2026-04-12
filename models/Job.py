2faadaed35fb37eebf78d18a1af547c6b7a8879a
loc <nguyenhtloc110@gmail.com>
2026-04-12 18:44:55 +0700

    feat: init project

---
+++ models/Job.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Job:
+    title: str
+    company: str
+    link: str
+    address: str
+    exp: str
+    salary: str = "Thỏa thuận"
+    posted_date: str = ""
+    image: str 
+
+    def to_discord_embed(self):
+        return {
+            "title": f"🚀 {self.title}",
+            "description": f"**Công ty:** {self.company}\n**Lương:** {self.salary}",
+            "LOGO": self.link, # Sử dụng link làm logo tạm thời
+            "url": self.link,
+            "color": 5814783, # Màu xanh dương
+            "footer": {"text": f"Nguồn: {self.source} | {self.posted_date}"}
+        }
