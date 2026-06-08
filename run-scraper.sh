3a35f1b1a41ae8b18bc423e646a32020e6a18bc5
loc <nguyenhtloc110@gmail.com>
2026-06-08 20:56:59 +0700

    feat: update project

--- run-scraper.sh
+++ run-scraper.sh
@@ -3,9 +3,6 @@
 # 1. Navigate to the absolute project root directory
 cd /home/loc/job-scraper || { echo "CRITICAL: Project directory not found!" && exit 1; }
 
-# 2. Inject pipeline secrets into environment variables
-export DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1493247072072106095/aUteZgerI29WJN0_UJtCmEu4Hh_RViv3DIahNj4rGF1J3kGHJmEhV6KWtKvpQu9lfweH"
-
 # 3. Execute the orchestrator directly using the virtual environment's binary
 # MẸO: Hãy chắc chắn file chạy tổng của bạn tên là run_scraper.py (file tụi mình vừa sửa) hay main.py nhé!
 /home/loc/venv/bin/python3 main.py
